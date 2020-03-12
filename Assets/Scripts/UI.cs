93d7a1f7d03cc40ad8507f496f552701fa96a8c8
Nezquik_lap <you@example.com>
2020-03-12 18:33:05 +0500

    add bombs and ui

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Bomb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bomb.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject _Bomb
+    {
+        get => Bomb;
+        set => Bomb = value;
+    }
+}
