1059d68e594ee205e2b7d760428e2f0fe9acd3df
Nezquik_lap <you@example.com>
2020-03-13 21:48:04 +0500

    refactor2

--- Assets/Scripts/Util/BombScript.cs
+++ Assets/Scripts/Util/BombScript.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 
 public class BombScript : MonoBehaviour
 {
-
-    private Animation animation;
     private float timeLeft;
+    private GameObject bomb;
+    private static List<GameObject> bombList = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
     {
-        animation = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
         timeLeft -= Time.deltaTime;
+        if(timeLeft<0 && bomb !=null)
+            Destroy(bomb);
     }
 
     public void Explode()
@@ -28,15 +29,7 @@ public class BombScript : MonoBehaviour
 
     public void Throw(float positionX, float positionY)
     {
-        
-        var b = Instantiate(gameObject, new Vector3(positionX, positionY, 0 ), Quaternion.identity);
-       
-        
-        animation.Play();
-      
-        Destroy(b, 3.0f);
-        
-       
-        
+        timeLeft = 3.0f;
+        bomb = Instantiate(gameObject, new Vector3(positionX, positionY, 0 ), Quaternion.identity);
     }
 }
