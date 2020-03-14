fb05cb113fa01a0e92f23a8e64b0aebf64a3e1d9
NezcafeGold <NezcafeGold@users.noreply.github.com>
2020-03-14 23:38:22 +0500

    edit last 2

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField] private int _Xsize;
+    [SerializeField] private int _Ysize;
+    [SerializeField] private int _ChangeOfTreasure;
+    [SerializeField] private int _Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int XSize
+    {
+        get => _Xsize;
+    }
+
+    public int YSize
+    {
+        get => _Ysize;
+   }
+
+    public int ChangeOfTreasure
+    {
+        get => _ChangeOfTreasure;
+    }
+
+    public int Speed
+    {
+        get => _Speed;
+        set => _Speed = value;
+    }
+}
